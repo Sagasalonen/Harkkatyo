2c86a9aaa8f25d0dee02f0d5c62d24299418fa84
Sagasalonen <saga.salonen@student.lut.fi>
2026-04-08 11:14:11 +0300

    Lisäsin Activityt MoveLutemons ja ListLutemons, sekä täydensin MainActivityä

--- app/src/main/java/com/example/harkkatyo/ListLutemons.java
+++ app/src/main/java/com/example/harkkatyo/ListLutemons.java
@@ -1,8 +1,6 @@
 package com.example.harkkatyo;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,28 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class ListLutemons extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_list_lutemons);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
     }
-    public void AddLutemons(View view){
-        Intent intent = new Intent(this, AddLutemons.class);
-    }
-    public void MoveLutemons(View view){
-        Intent intent = new Intent(this, MoveLutemons.class);
-    }
-    public void ListLutemons(View view){
-        Intent intent = new Intent(this, ListLutemons.class);
-    }
-    public void MoveToFightingArena(View view){}
-    public void MoveToTrainingArena(View view){}
 }
