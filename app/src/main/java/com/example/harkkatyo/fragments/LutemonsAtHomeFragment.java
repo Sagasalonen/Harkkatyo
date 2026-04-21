0023d37af0febb6ad0e8e4cfb5a3be313bbea781
jeliiko <jenna.korpiharju@student.lut.fi>
2026-04-21 14:37:02 +0300

    recycler view juttuja

--- app/src/main/java/com/example/harkkatyo/fragments/LutemonsAtHomeFragment.java
+++ app/src/main/java/com/example/harkkatyo/fragments/LutemonsAtHomeFragment.java
@@ -3,6 +3,7 @@ package com.example.harkkatyo.fragments;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -63,10 +64,10 @@ public class LutemonsAtHomeFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_lutemons_at_home, container, false);
+        RecyclerView recyclerView = view.findViewById(R.id.LutemonsAtHomeRV);
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_lutemons_at_home, container, false);
-
-        recyclerView = findViewById(R.id.LutemonsAtHomeRV);
+        return view;
     }
 
     public ArrayList<Lutemon> ListLutemonsAtHome(ArrayList<Lutemon> lutemons) {
